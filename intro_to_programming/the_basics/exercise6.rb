6eea253e258daf43b539f7fe928df516c10bc251
TacoMilitary <wlolams@outlook.com>
2025-12-07 20:18:32 -0600

    intro to programming (the basics) exercise 6

---
+++ intro_to_programming/the_basics/exercise6.rb
@@ -0,0 +1,9 @@
+def square(n)
+    n ** 2
+end
+
+3.times do
+    #random.new for max randomization
+    random_number = Random.new.rand(1..20)
+    puts "The square of #{random_number} is #{square(random_number)}"
+end
