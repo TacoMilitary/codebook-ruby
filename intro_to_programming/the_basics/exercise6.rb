31c3171a071307484b772456a6502898afb54751
TacoMilitary <wlolams@outlook.com>
2025-12-07 20:20:27 -0600

    fixed code to be random floats

--- intro_to_programming/the_basics/exercise6.rb
+++ intro_to_programming/the_basics/exercise6.rb
@@ -4,6 +4,6 @@ end
 
 3.times do
     #random.new for max randomization
-    random_number = Random.new.rand(1..20)
+    random_number = Random.new.rand(100..2000).to_f/100
     puts "The square of #{random_number} is #{square(random_number)}"
 end
