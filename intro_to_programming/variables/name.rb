2998ae0d33fb312dfd876a99b6fbd7651a0ee3ee
TacoMilitary <wlolams@outlook.com>
2025-12-09 18:56:20 -0600

    intro to programming (variables) exercise 4

--- intro_to_programming/variables/name.rb
+++ intro_to_programming/variables/name.rb
@@ -1,7 +1,13 @@
-puts "Hello, User! Please input your name!"
+puts "Hello, User! Please input your first name."
 
-user_input = gets.chomp
+user_first_name = gets.chomp
 
-puts "Welcome, to my program, #{user_input}!"
+puts "Welcome, to my program, #{user_first_name}!"
+
+puts "Please, enter your last name."
+
+user_last_name = gets.chomp
+
+puts "#{user_first_name} #{user_last_name} is such a cool name!"
 
 10.times {puts user_input.upcase + "!"}
