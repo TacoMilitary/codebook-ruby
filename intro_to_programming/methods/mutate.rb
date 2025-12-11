5c9c659367f487186b35ebe54b28f240e498536d
TacoMilitary <wlolams@outlook.com>
2025-12-10 21:31:56 -0600

    intro to programming (methods) no_mutate method

--- intro_to_programming/methods/mutate.rb
+++ intro_to_programming/methods/mutate.rb
@@ -1,8 +1,16 @@
 a = (1..3).to_a
 
-def mutating_method(array)
+def mutating_method(array=[])
 	puts "Removed elements: #{array.pop}"
 end
+
+def no_mutate(array=[])
+	puts "Return: #{array.last}"
+end
+
+puts "Before no_mutate: #{a}"
+no_mutate a
+puts "After no_mutate: #{a}"
 
 puts "Before mutation: #{a}"
 mutating_method a
