ebc4f2e22f5ddb2e9598acd1455333fa5872c7f1
TacoMilitary <wlolams@outlook.com>
2025-12-17 19:19:54 -0600

    intro to programming (flow control) exercise 2

---
+++ intro_to_programming/flow_control/exercise2.rb
@@ -0,0 +1,10 @@
+def upcase_short_strings(string_to_upcase)
+  string_to_upcase.upcase unless string_to_upcase.length <= 10
+  
+  #return changed or unchanged string
+  string_to_upcase
+end
+
+puts upcase_short_strings("hello world")
+puts upcase_short_strings("<= 10")
+puts upcase_short_strings("IS this LONGER than ten characters?")
