5c244999b84c026299cc7ab02ffa0a39359978fa
TacoMilitary <wlolams@outlook.com>
2025-12-17 19:41:39 -0600

    better exercise 2

--- intro_to_programming/flow_control/exercise2.rb
+++ intro_to_programming/flow_control/exercise2.rb
@@ -1,10 +1,12 @@
 def upcase_short_strings(string_to_upcase)
-  string_to_upcase.upcase! unless string_to_upcase.length <= 10
-  
-  #return changed or unchanged string
-  string_to_upcase
+  unless string_to_upcase.length <= 10
+    string_to_upcase.upcase
+  else
+    string_to_upcase
+  end
 end
 
 puts upcase_short_strings("hello world")
 puts upcase_short_strings("<= 10")
+puts upcase_short_strings("lessthan")
 puts upcase_short_strings("IS this LONGER than ten characters?")
